41625762ca13582373e87c23f2e1f900f2b8683b
moin152 <154750371+moin152@users.noreply.github.com>
2024-07-02 22:26:16 +0500

    Add files via upload

---
+++ Labs/lab1.py
@@ -0,0 +1,27 @@
+'''print("Hello")
+txt=input("Enter Your Name: ")
+if txt=="Moin Yaqoob":
+ print(txt)
+ print(type(txt))
+'''
+'''count=0
+while count<=3:
+    print("Hi")
+    count=count+1
+
+print ("list iteration")
+l= ["ggeks","for","geeks"]
+for i in l:
+    print(i)
+for index in range(len(l)):
+    print(index)
+for letter in 'geeksforgeeks':
+    if letter=='e'or letter=='s' :
+      break
+    print ('Current letter :', letter)
+    var=10
+    '''
+def my_fun():
+        print("Moin")
+
+my_fun()
